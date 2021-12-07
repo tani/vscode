e9af584edb81eb4835d8029130cb34a80bf24d84
deepak1556 <hop2deep@gmail.com>
2021-12-07 19:34:57 +0900

    fix: remove python usage in macos cli wrapper
    Fixes https://github.com/microsoft/vscode/issues/134635
    Fixes https://github.com/microsoft/vscode/issues/137954

--- resources/darwin/bin/code.sh
+++ resources/darwin/bin/code.sh
@@ -3,8 +3,16 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License. See License.txt in the project root for license information.
 
-function realpath() { python -c "import os,sys; print(os.path.realpath(sys.argv[1]))" "$0"; }
-CONTENTS="$(dirname "$(dirname "$(dirname "$(dirname "$(realpath "$0")")")")")"
+function realpath() {
+	SOURCE=$1
+	while [ -h "$SOURCE" ]; do
+  		DIR=$( dirname "$SOURCE" )
+  		SOURCE=$(readlink "$SOURCE")
+  		[[ $SOURCE != /* ]] && SOURCE=$DIR/$SOURCE
+	done
+	echo "$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
+}
+CONTENTS="$(dirname "$(dirname "$(dirname "$(realpath "${BASH_SOURCE[0]}")")")")"
 ELECTRON="$CONTENTS/MacOS/Electron"
 CLI="$CONTENTS/Resources/app/out/cli.js"
 ELECTRON_RUN_AS_NODE=1 "$ELECTRON" "$CLI" --ms-enable-electron-run-as-node "$@"
